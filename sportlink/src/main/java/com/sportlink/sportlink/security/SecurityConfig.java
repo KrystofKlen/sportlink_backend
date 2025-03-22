560bcba359ef7bd701c0a234ef84d0e1c1a46e42
krystof <krysklen@cvut.cz>
2025-03-22 20:10:50 +0100

    :sec: Adding https

--- sportlink/src/main/java/com/sportlink/sportlink/security/SecurityConfig.java
+++ sportlink/src/main/java/com/sportlink/sportlink/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
                 .authenticationProvider(accountAuthenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeRequests ->
